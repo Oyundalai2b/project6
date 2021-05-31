104181ff6aae8c344f5e023d60727f4f03d6f7d2
oyundalai2b <oyundalai2b@gmail.com>
2021-05-31 21:06:36 +1000

    add env variable

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const path = require("path");
 const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
 
 const app = express();
 
+dotenv.config();
+const MONGO_URL = `mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DB}`;
+console.log(MONGO_URL);
 mongoose
-  .connect("mongodb://localhost/project6")
+  .connect(MONGO_URL)
   .then(() => {
     console.log("Successfully connected to MongoDB");
   })
